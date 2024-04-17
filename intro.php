36912af7854d1b60527f8e2667f747bd76272a1a
samaptech <sameer2308b@aptechgdn>
2024-04-17 13:18:10 -0700

    complete php intro work

--- intro.php
+++ intro.php
@@ -21,9 +21,37 @@ var_dump($a);
 var_dump($b);
 var_dump($name);
 
-$fruit = array('apple', 'mango' , 'banana', )
+$fruits = array('apple', 'mango' , 'banana','orange','apricort');
+echo $fruits[0]."<br>";
+echo $fruits[1]."<br>";
+echo $fruits[2]."<br>";
+echo $fruits[3]."<br>";
+echo $fruits[4]."<br>";
+
+for($i=0; $i<5; $i++){
+    echo $i. " " .$fruits[$i]." ";
+}
 
 
+$j = 0;
+while($j<5){
+    echo $fruits[$j]." ";
+    $j++;
+}
+
+$a = 11;
+do{
+        echo $a;
+        $a++;
+}
+while($a <= 10);
+
+function average($num1 , $num2){
+    return($num1 + $num2)/2;
+}
+
+echo average(20 , 20)." ";
+echo average(50 , 50);
 
 
 ?>
