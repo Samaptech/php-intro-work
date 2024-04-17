0a67484e6bf874ef57c5e0e327b91f017865806f
samaptech <sameer2308b@aptechgdn>
2024-04-17 05:14:56 -0700

    GIT INTRO WORK HALF

---
+++ intro.php
@@ -0,0 +1,29 @@
+<?php
+echo "<h1>HI SAMEER</h1>";
+
+$a =  150;
+$b = 30;
+
+echo $a + $b ,"<br>";
+
+$name = "Sameer";
+echo $name . "<br>";
+
+$b = true;
+echo $b . "<br>";
+
+$weight = 15.2;
+echo $weight;
+
+var_dump($weight);
+var_dump($name);
+var_dump($a);
+var_dump($b);
+var_dump($name);
+
+$fruit = array('apple', 'mango' , 'banana', )
+
+
+
+
+?>
